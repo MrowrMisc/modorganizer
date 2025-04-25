769588d03d5a93d5ab78147ef5ee3f7c0748f8a6
Mrowr Purr <mrowr.purr@gmail.com>
2025-04-24 17:22:33 -0700

    checkpoint

--- game_oblivion_remastered.py
+++ game_oblivion_remastered.py
@@ -9,10 +9,80 @@ from ..basic_game import BasicGame
 class OblivionRemasteredModDataChecker(mobase.ModDataChecker):
     """
     Accept all mods regardless of structure to prevent MO2 from complaining.
+    Also restructures mods to put files in the correct directories.
     """
+    def __init__(self):
+        super().__init__()
+        # UE5 file extensions that should go in Paks/~mods/[mod name]
+        self.ue5_extensions = [".pak", ".ucas", ".utoc", ".uasset", ".uexp", ".ubulk", ".umap"]
+        # Bethesda file extensions that should go in Dev/ObvData/Data
+        self.bethesda_extensions = [".esp", ".bsa"]
+        # INI files that should go in Dev/ObvData
+        self.ini_files = ["blendsettings.ini", "oblivion.ini", "oblivion_default.ini"]
+    
     def dataLooksValid(self, filetree: mobase.IFileTree) -> mobase.ModDataChecker.CheckReturn:
         # Always return VALID to accept any mod structure
         return mobase.ModDataChecker.VALID
+    
+    def fix(self, filetree: mobase.IFileTree) -> mobase.IFileTree:
+        """
+        Restructure the mod to put files in the correct directories.
+        """
+        mod_name = filetree.name()
+        
+        # Check for UE5 files in the root
+        ue5_files_in_root = []
+        for entry in filetree:
+            if entry.isFile():
+                file_name = entry.name().lower()
+                file_ext = os.path.splitext(file_name)[1].lower()
+                if file_ext in self.ue5_extensions:
+                    ue5_files_in_root.append(entry)
+        
+        # Check for Bethesda files in the root
+        bethesda_files_in_root = []
+        for entry in filetree:
+            if entry.isFile():
+                file_name = entry.name().lower()
+                file_ext = os.path.splitext(file_name)[1].lower()
+                if file_ext in self.bethesda_extensions:
+                    bethesda_files_in_root.append(entry)
+        
+        # Check for INI files in the root
+        ini_files_in_root = []
+        for entry in filetree:
+            if entry.isFile():
+                file_name = entry.name().lower()
+                if file_name in self.ini_files:
+                    ini_files_in_root.append(entry)
+        
+        # If we found any files to move, restructure the mod
+        if ue5_files_in_root or bethesda_files_in_root or ini_files_in_root:
+            # Create the necessary directories
+            if ue5_files_in_root:
+                paks_dir = filetree.addDirectory("Paks")
+                mods_dir = paks_dir.addDirectory("~mods")
+                mod_dir = mods_dir.addDirectory(mod_name)
+                
+                # Move UE5 files to Paks/~mods/[mod name]
+                for entry in ue5_files_in_root:
+                    mod_dir.insert(entry, mobase.IFileTree.InsertPolicy.REPLACE)
+            
+            if bethesda_files_in_root or ini_files_in_root:
+                dev_dir = filetree.addDirectory("Dev")
+                obvdata_dir = dev_dir.addDirectory("ObvData")
+                
+                # Move INI files to Dev/ObvData
+                for entry in ini_files_in_root:
+                    obvdata_dir.insert(entry, mobase.IFileTree.InsertPolicy.REPLACE)
+                
+                # Create Data directory and move Bethesda files there
+                if bethesda_files_in_root:
+                    data_dir = obvdata_dir.addDirectory("Data")
+                    for entry in bethesda_files_in_root:
+                        data_dir.insert(entry, mobase.IFileTree.InsertPolicy.REPLACE)
+        
+        return filetree
 
 
 class OblivionRemasteredGame(BasicGame):
